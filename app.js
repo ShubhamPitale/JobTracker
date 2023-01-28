0f56c58c11b0792e296b0d0a6e0fcaef82305e5c
ShubhamPitale <shubhampitale44@gmail.com>
2023-01-28 16:50:31 +0530

    Solved a small issue

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-app.get('/', (req, res) => {
-  res.send('<h1>Job Api</h1><a href="/api-docs">Documentation</a>');
-});
+// app.get('/', (req, res) => {
+//   res.send('<h1>Job Api</h1><a href="/api-docs">Documentation</a>');
+// });
 
 //app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use(express.static(path.join(__dirname, './client/build')));
